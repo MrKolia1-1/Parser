b1473ab7ad1e3bdebf4d89b65db4a22e74b5437d
MrKolia1_1 <>
2022-08-17 11:32:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8500), 0);
+        HttpContext context = server.createContext("/");
+        context.setHandler(Main::handleRequest);
+        server.start();
+    }
+
+    private static void handleRequest(HttpExchange exchange) throws IOException {
+        String response = Runtime.getRuntime().toString();
+        exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
